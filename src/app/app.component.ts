5b54d1f2e1b025658c7aa64817044010a98b5b6a
Boye <boyeelhadjiabdou@gmail.com>
2025-01-09 12:44:29 +0000

    Modification sur les routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { EnteteComponent } from "./frame/entete/entete.component";
+import { MenuComponent } from "./frame/menu/menu.component";
+import { AccueilComponent } from "./accueil/accueil.component";
+import { FooterComponent } from "./frame/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, EnteteComponent, MenuComponent, AccueilComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
