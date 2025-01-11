0077adae2296eca8d3394282350bb2d41e28517b
Boye <boyeelhadjiabdou@gmail.com>
2025-01-11 09:16:28 +0000

    NGFor

--- src/app/liste-etudiant/liste-etudiant.component.ts
+++ src/app/liste-etudiant/liste-etudiant.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {JsonPipe, NgForOf} from "@angular/common";
+import {Etudiant} from "../ipsl_modele";
+import {Listes_etudiants} from "../Donnee";
 
 @Component({
   selector: 'app-liste-etudiant',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    JsonPipe
+  ],
   templateUrl: './liste-etudiant.component.html',
   styleUrl: './liste-etudiant.component.scss'
 })
 export class ListeEtudiantComponent {
-
+  etudiants= Listes_etudiants;
 }
